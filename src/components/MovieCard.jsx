760abc8d54979a64e734a260d5161e9b06a14be2
vivekrokadi <vivekrokadi@gmail.com>
2025-07-26 17:11:12 +0530

    adde movie details page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,33 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({
-  movie: { Title, Year, Poster, imdbRating, Language },
+  movie: { Title, Year, Poster, imdbRating, Language,imdbID },
 }) => {
   return (
-    <div className="movie-card">
-      <img
-        src={Poster && Poster !== "N/A" ? Poster : "/no-movie.png"}
-        alt={Title}
-        className="w-full h-80 object-cover rounded-lg"
-      />
+    <Link
+      to={`/movie/${imdbID}`}
+      className="hover:scale-[1.01] transition"
+    >
+      <div className="movie-card">
+        <img
+          src={Poster && Poster !== "N/A" ? Poster : "/no-movie.png"}
+          alt={Title}
+          className="w-full h-80 object-cover rounded-lg"
+        />
 
-      <div className="mt-4">
-        <h3 className="text-lg font-semibold">{Title}</h3>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold">{Title}</h3>
 
-        <div className="flex items-center gap-2 text-sm text-gray-500 mt-1">
-          <div className="flex items-center gap-1">
-            <img src="/star.svg" alt="star" className="w-4 h-4" />
-            <span>{imdbRating !== "N/A" ? imdbRating : "N/A"}</span>
+          <div className="flex items-center gap-2 text-sm text-gray-500 mt-1">
+            <div className="flex items-center gap-1">
+              <img src="/star.svg" alt="star" className="w-4 h-4" />
+              <span>{imdbRating !== "N/A" ? imdbRating : "N/A"}</span>
+            </div>
+
+            <span>•</span>
+            <p>{Language?.split(",")[0]}</p>
+
+            <span>•</span>
+            <p>{Year}</p>
           </div>
-
-          <span>•</span>
-          <p>{Language?.split(",")[0]}</p>
-
-          <span>•</span>
-          <p>{Year}</p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Spinner from "./Spinner";
+
+const API_BASE_URL = "https://www.omdbapi.com/";
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+function MovieDetails() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      const response = await fetch(
+        `${API_BASE_URL}?apikey=${API_KEY}&i=${id}&plot=full`
+      );
+      const data = await response.json();
+      setMovie(data);
+    };
+    fetchMovieDetails();
+  }, [id]);
+
+  if (!movie) {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <Spinner />
+    </div>
+  );
+}
+
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <button
+          onClick={() => navigate(-1)}
+          className="mb-4 text-3xl text-white self-start   font-bold"
+        >
+          &larr;
+        </button>
+        <div className="flex flex-col md:flex-row gap-6 mt-10">
+          <img
+            src={movie.Poster !== "N/A" ? movie.Poster : "/no-movie.png"}
+            alt={movie.Title}
+            className="w-full md:w-1/3 rounded shadow object-cover "
+          />
+          <div className="">
+            <h2 className="text-2xl sm:text-3xl font-bold">
+              {movie.Title} ({movie.Year})
+            </h2>
+            <p className="mt-2 text-xl text-gray-400">{movie.Plot}</p>
+            <ul className="mt-4 text-xl space-y-2 text-gray-400">
+              <li>
+                <strong>Rating:</strong> ⭐ {movie.imdbRating}
+              </li>
+              <li>
+                <strong>Genre:</strong> {movie.Genre}
+              </li>
+              <li>
+                <strong>Language:</strong> {movie.Language}
+              </li>
+              <li>
+                <strong>Director:</strong> {movie.Director}
+              </li>
+              <li>
+                <strong>Actors:</strong> {movie.Actors}
+              </li>
+              <li>
+                <strong>Runtime:</strong> {movie.Runtime}
+              </li>
+              <li>
+                <strong>BoxOffice collection:</strong> {movie.BoxOffice}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default MovieDetails;
